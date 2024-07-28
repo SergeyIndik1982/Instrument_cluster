869624375a6c984dac4df1f8264e067470be55ee
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2024-07-29 00:53:11 +0900

    Build Qt app for Raspberry Pi via Docker container and automate deploy process (#37)
    * chore(gitignore): add files to ignore
    
    * chore: add docker ignore
    
    * build: init CMakeLists
    
    * feat(UI): implement speedometer
    
    * build: build Qt app for Raspberry Pi
    
    * build: automate build and deploy process
    
    * fix: add missing variable

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Please alternate RASPBERRY_PI_HOST value when the ip adress of Raspberry Pi is changed
+
+# Variables
+DOCKER_IMAGE_NAME="qt-app-builder"
+DOCKER_CONTAINER_NAME="qt-app-build-container"
+BUILD_DIR="/project/build"
+HOST_COPY_DIR="."
+RASPBERRY_PI_USER="koserjinda"
+RASPBERRY_PI_HOST="192.168.86.38"
+RASPBERRY_PI_TARGET_DIR="/home/${RASPBERRY_PI_USER}"
+QT_APP_NAME="InstrumentCluster"
+
+echo "Building the Docker container..."
+docker build -t $DOCKER_IMAGE_NAME .
+
+echo "Running the Docker container..."
+docker run --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+
+echo "Copying the built application to the host machine..."
+docker cp $DOCKER_CONTAINER_NAME:$BUILD_DIR/$QT_APP_NAME $HOST_COPY_DIR
+
+echo "Removing the Docker container..."
+docker rm $DOCKER_CONTAINER_NAME
+
+echo "Transferring the built application to the Raspberry Pi..."
+scp -r $HOST_COPY_DIR/$QT_APP_NAME $RASPBERRY_PI_USER@$RASPBERRY_PI_HOST:$RASPBERRY_PI_TARGET_DIR
+
+echo "Removing executable file from host..."
+rm $HOST_COPY_DIR/$QT_APP_NAME
+
+echo "Done"
